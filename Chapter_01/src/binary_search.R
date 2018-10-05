e7d1b3dd48d0ca24a9d4062fff77a66317de99bf
jordan-singer <jordan.m.singer@gmail.com>
2018-10-05 14:57:24 -0500

    Add full code for binary search

--- Chapter_01/src/binary_search.R
+++ Chapter_01/src/binary_search.R
@@ -1,3 +1,20 @@
 binary_search = function(array, target) {
-  return(FALSE)
-}
+  left_bound = 1
+  right_bound = length(array)
+
+  while(left_bound < right_bound) {
+    check_index = as.integer((left_bound + right_bound) / 2)
+
+    if(array[check_index] == target) {
+      return(TRUE) }
+    else if(array[check_index] > target) {
+      right_bound = check_index }
+    else if(left_bound != check_index) {
+      left_bound = check_index }
+    else {
+      return(FALSE) } }
+
+  if(array[left_bound] == target) {
+    return(TRUE) }
+  else {
+    return(FALSE) } }
--- Chapter_01/src/binary_search.py
+++ Chapter_01/src/binary_search.py
@@ -1,2 +1,20 @@
 def binary_search(array, target):
+  left_bound = 0
+  right_bound = len(array) - 1
+
+  while left_bound < right_bound:
+    check_index = (left_bound + right_bound) // 2
+    
+    if array[check_index] == target:
+      return True
+    elif array[check_index] > target:
+      right_bound = check_index
+    elif left_bound != check_index:
+      left_bound = check_index
+    else:
+      return False
+
+  if array[left_bound] == target:
+    return True
+  else:
     return False
